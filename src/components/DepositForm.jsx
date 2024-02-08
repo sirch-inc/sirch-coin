eb86fc41dd86f38c2fa23311ca6efcdb3ed12548
Dimitri <dimitri.legaspi@gmail.com>
2024-02-08 01:55:54 -0500

    Updated Deposit Form

--- src/components/DepositForm.jsx
+++ src/components/DepositForm.jsx
@@ -1,152 +1,103 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 
-export default function DepositForm() {
-  const [selectedAmount, setSelectedAmount] = useState(null);
+const DepositForm = () => {
+  const [stripe, setStripe] = useState(null);
+  const [elements, setElements] = useState(null);
+  const [paymentIntent, setPaymentIntent] = useState(null);
+  const [clientSecret, setClientSecret] = useState(null);
+  const [containerVisible, setContainerVisible] = useState(true);
+  const [successVisible, setSuccessVisible] = useState(false);
   const [email, setEmail] = useState("");
-  const [coinAmount, setCoinAmount] = useState("");
-  const [totalUSD, setTotalUSD] = useState("Total USD");
+  const [amount, setAmount] = useState(0);
+  const paymentElementRef = useRef(null);
+  
+  useEffect(() => {
+    // Load stripe and set it in state
+    const stripeKey = "pk_test_6rOaG7p9vtW2VyduXtVfr7JV00sqg9HpxQ";
+    loadStripe(stripeKey).then(setStripe);
+  }, []);
 
-  const handleAmountButtonClick = (amount) => {
-    setSelectedAmount(amount);
-    setCoinAmount(`${(amount / 0.1).toFixed(2)}`);
-    setTotalUSD(`$${amount.toFixed(2)} USD`);
-  };
-
-  const emailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleCoinInputChange = (event) => {
-    setCoinAmount(event.target.value);
-    setSelectedAmount((event.target.value * 0.1).toFixed(2));
-    setTotalUSD((event.target.value * 0.1).toFixed(2));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = {
-      email: email,
-      numberOfCoins: coinAmount,
-      selectedAmount: selectedAmount,
-    };
-
-    try {
-      const intializePaymentUrl =
-        "https://sirchcoinv1-production.up.railway.app/api/v1/payments/initialize-payment";
-      const fetchConfig = {
+  const initializeStripe = async () => {
+    const response = await fetch(
+      "https://sirchcoinv1-production.up.railway.app/api/v1/payments/initialize-payment",
+      {
         method: "POST",
-        body: JSON.stringify(data),
         headers: {
           "Content-Type": "application/json",
         },
-      };
-
-      const response = await fetch(intializePaymentUrl, fetchConfig);
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        body: JSON.stringify({
+          numberOfCoins: amount,
+          email,
+        }),
       }
-
-      setEmail("");
-      setCoinAmount(0);
-      setTotalUSD("Total USD");
-    } catch (error) {
-      console.error("Error during fetch:", error);
+    );
+    const data = await response.json();
+    if (data.clientSecret) {
+      setClientSecret(data.clientSecret);
+      if (stripe) {
+        const elements = stripe.elements();
+        setElements(elements);
+        const card = elements.create("card"); // Create an instance of the card Element
+        card.mount(paymentElementRef.current); // Mount the Element to the DOM
+      }
+    } else {
+      console.error("Failed to initialize payment");
     }
   };
+
+  const handleClick = async () => {
+    if (!stripe || !elements) {
+      await initializeStripe();
+    } else {
+      const result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement('card'), // Use the card Element
+          billing_details: {
+            email: email,
+          },
+        },
+      });
+      if (result.error) {
+        // Show error to your customer (e.g., insufficient funds)
+        console.error(result.error.message);
+      } else {
+        if (result.paymentIntent.status === 'succeeded') {
+          setSuccessVisible(true);
+          setContainerVisible(false);
+          setPaymentIntent(result.paymentIntent);
+        }
+      }
+    }
+  };
+
   return (
-    <>
-      <h3 className="page-header">Buy Sirch Coins</h3>
-      <div className="buy-container">
-        <p className="page-text">
-          Please enter an amount in USD that you'd like to spend buying Sirch
-          Coins, and then press <strong>Buy</strong>.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <div className="price-container">
-            <div className="first-row">
-              <button
-                className="cash-btn"
-                onClick={() => handleAmountButtonClick(20)}
-              >
-                $20
-              </button>
-              <button
-                className="cash-btn"
-                onClick={() => handleAmountButtonClick(40)}
-              >
-                $40
-              </button>
-              <button
-                className="cash-btn"
-                onClick={() => handleAmountButtonClick(100)}
-              >
-                $100
-              </button>
-              <input
-                placeholder="Amount of Coin"
-                required
-                type="text"
-                name="coin"
-                id="coin"
-                className="cash-input"
-                value={coinAmount}
-                onChange={handleCoinInputChange}
-              />
-            </div>
-            <div className="second-row">
-              <button
-                className="cash-btn"
-                onClick={() => handleAmountButtonClick(1000)}
-              >
-                $1000
-              </button>
-              <button
-                className="cash-btn"
-                onClick={() => handleAmountButtonClick(500)}
-              >
-                $500
-              </button>
-              <button
-                className="cash-btn"
-                onClick={() => handleAmountButtonClick(239)}
-              >
-                $239
-              </button>
-              <input
-                placeholder="Your Email"
-                required
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                value={email}
-                onChange={emailChange}
-                className="cash-input"
-              />
-            </div>
-            <input
-              placeholder="Total USD"
-              readOnly
-              type="text"
-              name="totalUSD"
-              id="totalUSD"
-              className="total-input"
-              value={totalUSD}
-              onChange={handleCoinInputChange}
-            />
-          </div>
-          <div className="bottom-btn-container">
-            <Link to="/" className="big-btn-red">
-              Back
-            </Link>
-            <button type="submit" className="big-btn-blue">
-              Buy
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
+    <div>
+      {containerVisible && (
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Amount:</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <div ref={paymentElementRef} />
+          <button onClick={handleClick}>
+            Submit
+          </button>
+        </div>
+      )}
+      {successVisible && (
+        <div className="success">Payment succeeded! {paymentIntent && paymentIntent.id}</div>
+      )}
+    </div>
   );
-}
+};
+
+export default DepositForm;
