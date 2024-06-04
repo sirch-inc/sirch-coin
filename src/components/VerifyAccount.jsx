27b77068d7fa2c25729dd09230abebdfdee495d4
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-06-04 15:29:17 -0600

    minor TODO inclusion for verify acct

--- src/components/VerifyAccount.jsx
+++ src/components/VerifyAccount.jsx
@@ -2,6 +2,7 @@ export default function VerifyAccount() {
 
     return(
         // TODO: Style component and add customization (ie. "An email confirmation link was sent to [usersEmail@whaterver.com")
+        // TODO: Only make page accessible to newly created user
         <>
             <h1>Check Your Email</h1>
             <h4>We sent a verification email to the email address you provided. Click the link in your email to login to your new account.</h4>
