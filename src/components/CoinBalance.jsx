e0245dcd8def0dcc03adf887259547440790dcd1
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-13 15:34:28 -0600

    rework Get Balance component

--- src/components/CoinBalance.jsx
+++ src/components/CoinBalance.jsx
@@ -1,76 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+import supabase from '../Config/supabaseConfig';
 
-export default function CoinBalance() {
-  const [email, setEmail] = useState("");
-  const [balance, setBalance] = useState(null);
+
+const CoinBalance = () => {
+  const { userBalance, userInTable } = useContext(AuthContext);
+  const [currentBalance, setCurrentBalance] = useState(null);
   const [error, setError] = useState("");
 
-  const emailChange = (event) => {
-    setEmail(event.target.value);
+  const fetchUserBalance = async (userInTable) => {
+    if (userInTable) {
+      const { data, error } = await supabase
+        .from('user-balances')
+        .select("*")
+        .eq('user_id', userInTable.user_id)
+        .single();
+
+      if (error) {
+        // TODO: handle error
+        alert('Error fetching user balance:', error);
+      } else {
+        // TODO: handle NULL or otherwise malformed balances
+        setCurrentBalance(data.balance);
+      }
+    } else {
+      // TODO: handle no userInTable
+      alert('User not found');
+    }
   };
 
-  const fetchBalance = async (event) => {
+  useEffect(() => {
+    // (Re)fetch the user's balance when the component mounts
+    fetchUserBalance(userInTable);
+  }, [userBalance]);
+
+  const onRefreshBalance = async (event) => {
     event.preventDefault();
     setError("");
-
-    if (!email) {
-      setError("Email is required to fetch the balance.");
-      return;
-    }
-
-    const fetchUrl = `https://sirchcoinv1-production.up.railway.app/api/v1/customers/balance?email=${email}`;
-    const fetchConfig = {
-      // ❗❗❗ Changed mode to 'no-cors'
-      mode: 'no-cors',
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-
-    try {
-      const response = await fetch(fetchUrl, fetchConfig);
-      if (response.ok) {
-        const result = await response.json();
-        setBalance(result.coinBalance);
-      } else {
-        const errorMessage = "Invalid Email";
-        setError(errorMessage);
-      }
-    } catch (error) {
-      setError(`An error occurred: ${error.message}`);
-    }
+    fetchUserBalance(userInTable);
   };
+
+  let currentBalanceString = currentBalance !== null
+    ?  currentBalance + " Sirch Coins"
+    :  "Loading...";
 
   return (
     <>
       <h3 className="page-header">Balance Inquiry</h3>
       <div className="balance-container">
-        <p className="page-text">
-          Review your balance information, then press <strong>Buy More</strong>{" "}
-          when you are done.
-        </p>
-        <form onSubmit={fetchBalance}>
-          <input
-            placeholder="Enter your Email"
-            required
-            type="email"
-            name="email"
-            id="email"
-            className="email-balance"
-            value={email}
-            onChange={emailChange}
-            autoComplete="email"
-          />
-          <button className="balance-btn" type="submit">
-            Get Balance
-          </button>
-          {error && <p className="text-danger">{error}</p>}
-          {balance !== null && (
-            <h4 className="balance">${balance} Sirch Coins</h4>
-          )}
-        </form>
+        <div>
+          <h2>You currently have a balance of:</h2>
+          <h4>{currentBalanceString}</h4>
+        </div>
+        <div>
+          <button className="balance-btn" onClick={onRefreshBalance}>Refresh Balance</button>
+        </div>
+        {error && <p className="text-danger">{error}</p>}
         <div className="bottom-btn-container">
           <Link to="/" className="big-btn-red">
             Back
@@ -83,3 +69,5 @@ export default function CoinBalance() {
     </>
   );
 }
+
+export default CoinBalance;
