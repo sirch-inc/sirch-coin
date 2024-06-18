253b5ec1609ae0e3f6d1d6eb1dfc5b2a211a8ea1
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-18 17:37:54 -0600

    Update MainPage.jsx

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,41 +1,42 @@
 import { Link } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 import { AuthContext } from "./components/AuthContext";
 import { useEffect, useContext } from "react";
 import supabase from "./Config/supabaseConfig"
 // import { useAuth0 } from "@auth0/auth0-react";
-import { jwtDecode } from "jwt-decode"
+// import { jwtDecode } from "jwt-decode"
 
 
 export default function MainPage() {
-  const { userInTable } = useContext(AuthContext);
+  const { session, userInTable } = useContext(AuthContext);
 
-useEffect(() => {
-  const urlParams = new URLSearchParams(window.location.hash.slice(1));
-  const type = urlParams.get('type');
-  const accessToken = urlParams.get('access_token');
+  // useEffect(() => {
+  //   const urlParams = new URLSearchParams(window.location.hash.slice(1));
+  //   const type = urlParams.get('type');
+  //   const accessToken = urlParams.get('access_token');
 
-  const verifiedEmailPopup = async () => {
-    if (type && accessToken){
-      if (type === 'signup' && accessToken) {
-        toast.success("Success! Your Sirch Coin account has been verified.", {
-          position: "top-center",
-          autoClose: 3000, 
-        })
-      } else if (type === null || accessToken === null) {
-        toast.error("Email verification failed. Please try again.", {
-          position: "top-center",
-          autoClose: 3000,
-        })
-      }
-    } else {
-      console.log("Type: ", type)
-      console.log("AccessToken: ", accessToken)
-    }
-    verifiedEmailPopup();
-  }
-})
+  //   const verifiedEmailPopup = async () => {
+  //     if (type !== null && accessToken !== null){
+  //       if (type === 'signup' && accessToken !== null) {
+  //         toast.success("Success! Your Sirch Coin account has been verified.", {
+  //           position: "top-center",
+  //           autoClose: 3000, 
+  //         })
+  //       } else if (type === null || accessToken === null) {
+  //         toast.error("Email verification failed. Please try again.", {
+  //           position: "top-center",
+  //           autoClose: 3000,
+  //         })
+  //       }
+  //     } else {
+  //       console.log("some other error?");
+  //       console.log("Type: ", type)
+  //       console.log("AccessToken: ", accessToken)
+  //     }
+  //     verifiedEmailPopup();
+  //   }
+  // })
   
   // Display success messaging when user verifies their account via email
   // useEffect(() => {
@@ -86,42 +87,48 @@ useEffect(() => {
 
   return (
     <>
-        {userInTable ? (
-          <h3 className="page-header">Welcome, {userInTable.name}!</h3>
-        ) : (
-          <h3 className="page-header"> Welcome! Please sign in to use Sirch Coins.</h3>)}
+      {session && userInTable ? (
+        <h3 className="page-header">Welcome, {userInTable.name}!</h3>
+      ) : (
+        <h3 className="page-header"> Welcome! Please sign in to use Sirch Coins.</h3>
+      )}
       
-      <div className="button-container">
+      {session ? (
+        <div className="button-container">
+          <Link to="/coin-faucet" className="action-btn">
+              Internal: Balance & Faucet
+          </Link>
 
-      <Link to="/coin-faucet" className="action-btn">
-          Internal: Balance & Faucet
-      </Link>
+          <Link to="coin/send" className="action-btn">
+            Send Money
+          </Link>
 
-        <Link to="coin/send" className="action-btn">
-          Send Money
-        </Link>
+          <Link to="checkout" className="action-btn">
+            Deposit
+          </Link>
 
-        <Link to="checkout" className="action-btn">
-          Deposit
-        </Link>
+          <Link to="coin/balance" className="action-btn">
+            Balance Inquiry
+          </Link>
 
-        <Link to="coin/balance" className="action-btn">
-          Balance Inquiry
-        </Link>
+          <Link to="/transferhistory" className="action-btn">
+            Transfer History
+          </Link>
 
-        <Link to="/transferhistory" className="action-btn">
-          Transfer History
-        </Link>
+          <Link to="preferences" className="action-btn">
+            Preferences
+          </Link>
 
-        <Link to="preferences" className="action-btn">
-          Preferences
-        </Link>
-
-        <Link to="help" className="action-btn">
+          <Link to="help" className="action-btn">
+            Help
+          </Link>
+        </div>
+        ) : (
+          <Link to="help" className="action-btn">
           Help
-        </Link>
-      </div>
-      <ToastContainer />
+          </Link>
+        )}
+      {/* <ToastContainer /> */}
     </>
   );
 }
