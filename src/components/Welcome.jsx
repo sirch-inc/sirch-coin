c90b8e6ffc7f02ccaa7845cdc2d90316db2bdda5
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-06-20 14:56:05 -0600

    Add verifying... h1 while awaiting user data

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,16 +6,21 @@ import { useContext } from "react";
 export default function Welcome() {
   const { session, userInTable } = useContext(AuthContext);
 
+
     return(
         <>
           {session && userInTable ? 
-            <h1>Welcome {userInTable?.name}!</h1> : <> </> }
+          <div>
+            <h1>Welcome {userInTable?.name}!</h1>
             <h4>Your SirchCoins account has been created and you may now use all of the SirchCoin services!</h4>
             <div className="button-container">
               <Link to="/" className="action-btn">
                 Get Started!
               </Link>
             </div>
+          </div>
+             : <h1> Verifying... </h1> }
+            
         </>
     )
 }
