55857a98504ea5c1773e962d06cfd6e267f638fa
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-06-25 10:43:22 -0600

    setup calls for sent and received transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useContext,useEffect } from "react";
 import { Link } from "react-router-dom";
 import TransactionCard from "./TransactionCard";
-
+import { AuthContext } from "./AuthContext";
+import supabase from '../Config/supabaseConfig';
 
 export default function Transactions() {
+  const { userInTable } = useContext(AuthContext);
+
+  const fetchUserSentTransactions = async (userInTable) => {
+    if (userInTable){
+      const { data, error } = await supabase
+      .from("transactions")
+      .select("*")
+      .eq('sender_id', userInTable.user_id);
+
+      if (error) {
+        //TODO: handle error
+        alert('Error fetching users sent transactions: ', error);
+      } else {
+        console.log(data)
+      }
+    }
+    else{
+      // alert('User not found.')
+      console.log("No user in table")
+    }
+  }
+
+  const fetchUserReceivedTransactions = async (userInTable) => {
+    if (userInTable) {
+      const { data, error } = await supabase
+      .from("transactions")
+      .select("*")
+      .eq('receiver_id', userInTable.user_id);
+
+      if (error){
+        //TODO: handle error 
+        alert('Error fetching users received transactions', error);
+      } else {
+        console.log(data)
+      }
+    } else {
+      // alert('User not found.')
+      console.log("No user in table")
+    }
+  }
+  fetchUserSentTransactions(userInTable);
+  fetchUserReceivedTransactions(userInTable);
+
   return (
     <>
       <h3 className="page-header">Transaction History</h3>
