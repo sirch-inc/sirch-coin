ca585158b4ed40dcbae4af118189097a01708f40
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-26 16:11:30 -0600

    convert tabs to spaces

--- src/components/TransferCard.jsx
+++ src/components/TransferCard.jsx
@@ -2,25 +2,25 @@ import { parseISO, formatDistanceToNow } from 'date-fns';
 
 
 export default function TransferCard({ date, target, amount }) {
-    const formatDate = (unformattedDate) => {
-        const formattedDate = parseISO(unformattedDate);
-        return formatDistanceToNow(
-            formattedDate,
-            { addSuffix: true }
-        );          
-    }
+  const formatDate = (unformattedDate) => {
+    const parsedDate = parseISO(unformattedDate);
+    return formatDistanceToNow(
+      parsedDate,
+      { addSuffix: true }
+    );          
+  };
 
-    return (
-        <>
-            <div>
-                <p>{formatDate(date)}</p>
-            </div>
-            <div>
-                <p>{target}</p>
-            </div>
-            <div>
-                <p>{amount}</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <p>{formatDate(date)}</p>
+      </div>
+      <div>
+        <p>{target}</p>
+      </div>
+      <div>
+        <p>{amount}</p>
+      </div>
+    </>
+  );
 }
