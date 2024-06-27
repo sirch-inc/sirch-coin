51f393013ad50176429f6237c4e025903a8ac617
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-26 20:02:27 -0600

    delint StripeFailure

--- src/components/Stripe/StripeFailure.jsx
+++ src/components/Stripe/StripeFailure.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 
@@ -13,4 +12,4 @@ export default function StripeFailure() {
       </Link>
     </div>
   );
-};
+}
