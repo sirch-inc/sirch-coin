8fb852440b0c5f278ad626a607239e1eef024067
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-26 19:53:35 -0600

    delint VerifyAccount

--- src/components/Account/VerifyAccount.jsx
+++ src/components/Account/VerifyAccount.jsx
@@ -1,10 +1,10 @@
 export default function VerifyAccount() {
-    return(
-        // TODO: Style component and add customization (ie. "An email confirmation link was sent to [usersEmail@whaterver.com")
-        // TODO: Only make page accessible to newly created user
-        <>
-            <h1>Please Check Your Email</h1>
-            <h4>A verification email was sent to the address provided. Please click the link in that email to verify your new account.</h4>
-        </>
-    )
+  return(
+    // TODO: Style component and add customization (ie. "An email confirmation link was sent to [usersEmail@whaterver.com")
+    // TODO: Only make page accessible to a newly created user
+    <>
+      <h1>Please Check Your Email</h1>
+      <h4>A verification email was sent to the address provided. Please click the link in that email to verify your new account.</h4>
+    </>
+  )
 }
