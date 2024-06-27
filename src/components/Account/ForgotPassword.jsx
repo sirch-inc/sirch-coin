6c206ab2822999276fc256ac332133308bf562f5
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-26 18:44:53 -0600

    delint ForgotPassword

--- src/components/Account/ForgotPassword.jsx
+++ src/components/Account/ForgotPassword.jsx
@@ -3,56 +3,56 @@ import supabase from "../../Config/supabaseConfig"
 
 
 export default function ForgotPassword() {
-    const [userEmail, setUserEmail] = useState("");
-    const [resetSent, setResetSent] = useState(false);
-    const [error, setError] = useState(null);
-    
-    async function requestReset(e){
-        e.preventDefault();
+  const [userEmail, setUserEmail] = useState("");
+  const [resetSent, setResetSent] = useState(false);
+  const [error, setError] = useState(null);
+  
+  async function requestReset(e) {
+    e.preventDefault();
 
-        // Use supabase resetPasswordForEmail() to trigger a password reset email event and redirect to the update password page
-        try {
-            const {data, error} = await supabase.auth.resetPasswordForEmail(userEmail, {
-                redirectTo: `${window.location.origin}/reset-password`, })
-
-            if (error) {
-              // TODO: surface or handle error
-                throw error;
-            } else {
-                setResetSent(true);
-            }
-        }
-
-        catch (error) {
-            // TODO: surface or handle error...
-            console.error('Error: ', error.message);
-            setError(error.message);
-        }
-    }
-
-    return (
-        <>
-          <h1>Reset Password</h1>
-          {/* If the reset hasn't been sent, display the form. Otherwise show "sent" messaging.*/}
-          {!resetSent ? (
-            <>
-              <p>Enter the email you used to sign up for Sirch Coin:</p>
-              <form className="reset-password" onSubmit={requestReset}>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  value={userEmail}
-                  onChange={(e) => setUserEmail(e.target.value)}
-                  required
-                  autoComplete="username"
-                />
-                <button type="submit">Send Reset Email</button>
-              </form>
-              {error && <p className="error">{error}</p>}
-            </>
-          ) : (
-            <p>If the email address {userEmail} has a Sirch Coins account, we've emailed you a link to reset your password. Please check your email inbox.</p>
-          )}
-        </>
+    try {
+      const {data, error} = await supabase.auth.resetPasswordForEmail(
+        userEmail,
+        { redirectTo: `${window.location.origin}/reset-password` }
       );
+
+      if (error) {
+        // TODO: surface or handle error...
+        console.log("Data", data);
+        throw error;
+      } else {
+        setResetSent(true);
+      }
+    } catch (exception) {
+      // TODO: surface or handle error...
+      console.error('An exception occurred: ', exception.message);
+      setError(exception.message);
+    }
+  }
+
+  return (
+    <>
+      <h1>Reset Password</h1>
+      {/* TODO: Does this boolean switch even apply anymore since we now redirect on success of the request? */}
+      {!resetSent ? (
+      <>
+        <p>Enter the email you used to sign up for Sirch Coin:</p>
+        <form className="reset-password" onSubmit={requestReset}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
+            autoComplete="username"
+          />
+          <button type="submit">Send Reset Email</button>
+        </form>
+        {error && <p className="error">{error}</p>}
+      </>
+      ) : (
+      <p>If the email address {userEmail} has a Sirch Coins account, we&apos;ve emailed you a link to reset your password. Please check your email inbox.</p>
+      )}
+    </>
+  );
 }
