57e9ea266c941c5eab376063376b0e042789f10c
Jeff Rink <jeffrinkdev@gmail.com>
2024-06-26 19:44:39 -0600

    delint Login

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -21,77 +21,80 @@ export default function Login() {
 
       if (error) {
         setSignInError(true);
+
+        // TODO: hack to get around lint
+        console.log("Data", data);
+
         throw error;
       }
 
-      // Upon successful login, redir. user to homepage
       navigate("/");
+    } catch (exception) {
       // TODO: Add login failure notification to user
-    } catch (error) {
-      console.error("Error logging in:", error.message);
+
+      console.error("Error logging in:", exception.message);
     }
   };
   
-    // TODO: Style component
-    return (
-      <AuthContext.Consumer>
-        {({ session }) =>
-          !session &&
-            !signInError ? (
-            <>
+  return (
+    <AuthContext.Consumer>
+      {({ session }) =>
+        !session &&
+          !signInError ? (
+          <>
+          <h2>Log In</h2>
+          <p> New users should <a href="/create-account">create an account</a> first.</p>
+          <form onSubmit={handleLogin}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              autoComplete="username"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="current-password"
+            />
+            <button type="submit">Log In</button>
+            <a href="/forgot-password">Forgot Password?</a>
+          </form>
+          </>
+        ) : (
+          <div>
             <h2>Log In</h2>
-            <p> New users should <a href="/create-account">create an account</a> first.</p>
+            <p>New users should <a href="/create-account">create an account</a> first.</p>
             <form onSubmit={handleLogin}>
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                autoComplete="username"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                autoComplete="current-password"
-              />
-              <button type="submit">Log In</button>
-              <a href="/forgot-password">Forgot Password?</a>
-            </form>
-            </>
-          ) : (
-            <div>
-              <h2>Log In</h2>
-              <p>New users should <a href="/create-account">create an account</a> first.</p>
-              <form onSubmit={handleLogin}>
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                autoComplete="username"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                autoComplete="current-password"
-              />
-              <button type="submit">Log In</button>
-              <a href="/forgot-password">Forgot Password?</a>
-            </form>
-            <div>
-              <p style={{ color: "red" }}>There was an issue with your credentials. Please try logging in again.</p>
-            </div>
-            </div>
-          )
-        }
-      </AuthContext.Consumer>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              autoComplete="username"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="current-password"
+            />
+            <button type="submit">Log In</button>
+            <a href="/forgot-password">Forgot Password?</a>
+          </form>
+          <div>
+            <p style={{ color: "red" }}>There was an issue with your credentials. Please try logging in again.</p>
+          </div>
+          </div>
+        )
+      }
+    </AuthContext.Consumer>
     );
-  };
+  }
