c09a9667cdf55da5bc17490bc540713991c2a2c6
Jeff Rink <jeffrinkdev@gmail.com>
2024-08-16 11:59:35 -0600

    minor change to prelaunch warning

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./components/App/App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-if (import.meta.env.PROD)
+if (import.meta.env.MODE === 'production')
 {
   alert("PRELAUCH WARNING:\n\nThis is the public production Sirch Coin site.\n\n\
 Only create real accounts here, and use real credit cards.\n\nPurchases are ACTUAL.");  
