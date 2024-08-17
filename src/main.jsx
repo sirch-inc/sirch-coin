85dfe50db8f6f1f8b31f02e35213db4ba9d37068
Jeff Rink <jeffrinkdev@gmail.com>
2024-08-17 15:33:57 -0600

    update warning

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 if (import.meta.env.MODE === 'production')
 {
-  alert("PRELAUCH WARNING:\n\nThis is the public production Sirch Coin site.\n\n\
-This environment is currently still using the TEST version of the backend (and DB).\n\nUse TEST credit-cards for purchases.");  
+  alert("PRE-ALPHA WARNING:\n\nThis is the pre-alpha public production Sirch Coin site.\n\n\
+Transactions are real and recorded.\n\n\
+You must use real credit cards for purchases at this time, which will be processed and debited.\n\n\
+If you require additional support or adjustments to your balance or transaction history, contact us."
+);
 }
 
 document.title = import.meta.env.VITE_PAGE_TITLE;
