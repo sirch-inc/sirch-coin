aa66ae302a889d7450b089c7227b91a222f050fa
Jeff Rink <jeffrinkdev@gmail.com>
2024-08-17 21:19:41 -0600

    update verification page as requested

--- src/components/Account/VerifyAccount.jsx
+++ src/components/Account/VerifyAccount.jsx
@@ -5,6 +5,7 @@ export default function VerifyAccount() {
     <>
       <h1>Please Check Your Email</h1>
       <h4>A verification email was sent to the address provided. Please click the link in that email to verify your new account.</h4>
+      <h4>To ensure your account&apos;s security, this process may take a few minutes.</h4>
     </>
   )
 }
