9d6939b6dd5409ba046446f23dd491b60f67346b
Jeff Rink <jeffrinkdev@gmail.com>
2024-09-03 15:46:50 -0600

    switch to using more auth events

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -8,31 +8,62 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [session, setSession] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
   const [userInTable, setUserInTable] = useState(null);
   const [userBalance, setUserBalance] = useState(null);
 
   // Authenticate users
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session }}) => {
-      setSession(session);
+    // TODO: commented this out for now in favor of using supabase Auth events below...do we need this??
+    // supabase.auth.getSession().then(({ data: { session }}) => {
+    //   setSession(session);
 
-      if (session && session.user) {
-        setUserId(session.user.id);
+    //   if (session && session.user) {
+    //     setUserId(session.user.id);
+    //   }
+    // });
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+      // TODO: handle the events appropriately
+      switch (event) {
+        case 'INITIAL_SESSION':
+        case 'SIGNED_IN':
+        case 'USER_UPDATED':
+          setSession(session);
+
+          if (session && session.user) {
+            setUserId(session.user.id);
+            setUserEmail(session.user.email);
+          }
+          break;
+        case 'SIGNED_OUT':
+          setSession(null);
+          setUserId(null);
+          setUserEmail(null);
+          setUserInTable(null);
+
+          // clear local and session storage
+          [
+            window.localStorage,
+            window.sessionStorage,
+          ].forEach((storage) => {
+            Object.entries(storage)
+              .forEach(([key]) => {
+                storage.removeItem(key)
+              })
+          });
+          break;
+        case 'PASSWORD_RECOVERY':
+          setSession(session);
+          break;
+        case 'TOKEN_REFRESHED':
+          setSession(session);
+          break;
+        default:
+          console.error("Auth - Unknown Event", event);
+          break;
       }
     });
-
-    const { data: { subscription } } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
-        setSession(session);
-
-        if (session && session.user) {
-          setUserId(session.user.id);
-        } else {
-          setUserId(null);
-          setUserInTable(null);
-        }
-      }
-    );
 
     return () => subscription.unsubscribe();
   }, []);
@@ -49,7 +80,7 @@ export const AuthProvider = ({ children }) => {
 
         if (error) {
           // TODO: surface this error
-          alert('Error checking user in table:', error);
+          alert('Error finding user in table:\n' + error);
         } else {
           setUserInTable(data);
         }
@@ -58,7 +89,7 @@ export const AuthProvider = ({ children }) => {
 
     checkUserInTable();
   }, [userId]);
-  
+    
   // Get user's current balance
   useEffect(() => {
     const getUserBalance = async () => {
@@ -71,7 +102,7 @@ export const AuthProvider = ({ children }) => {
 
         if (error) {
           // TODO: surface this error...
-          alert("Error checking this user's balance:", error);
+          alert("Error checking this user's balance:\n" + error);
         } else {
           setUserBalance(data);
         }
@@ -81,9 +112,8 @@ export const AuthProvider = ({ children }) => {
     getUserBalance();
   }, [userInTable]);
   
-
   return (
-    <AuthContext.Provider value={{ session, userId, userInTable, userBalance }} supabase={ supabase }>
+    <AuthContext.Provider value={{ session, userId, userEmail, userInTable, userBalance }} supabase={ supabase }>
       {children}
     </AuthContext.Provider>
   )
