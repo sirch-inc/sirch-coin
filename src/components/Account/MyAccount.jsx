7afe6c96d322228fc9c505e7814b63376f11283d
Jeff Rink <jeffrinkdev@gmail.com>
2024-09-09 16:49:58 -0600

    mostly working again

--- src/components/Account/MyAccount.jsx
+++ src/components/Account/MyAccount.jsx
@@ -1,7 +1,9 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthContext';
-import supabase from '../App/supabaseConfig'; 
+import supabase from '../App/supabaseConfig';
+import { ToastContainer, toast } from 'react-toastify';
+import Logout from "../Account/Logout";
 
 
 export default function MyAccount(){
@@ -12,7 +14,7 @@ export default function MyAccount(){
 
   const navigate = useNavigate();
 
-  function showDeleteConfirmation(){
+  function showDeleteConfirmation() {
     setDeleteDialogBox(true);
   }
 
@@ -27,8 +29,9 @@ export default function MyAccount(){
     setIsUserHandleVerified(value === userInTable.user_handle);
   };
 
-  // TODO: Finish and test functions - on hold pending cascade discussions
-  function handleDeleteUser() {    
+  function handleDeleteUser() {  
+    event.preventDefault();
+  
     const deleteUser = async () => {
       if (userInTable) {
         const { error } = await supabase.functions.invoke('delete-auth-user', {
@@ -38,13 +41,13 @@ export default function MyAccount(){
         });
 
         if (error) {
-          // TODO: surface error
-          alert("Error deleting your account:\n" + error)
-        } else {
-          // TODO: surface alert notification with Toast
-          navigate('/')
-          alert("Your account has been deleted.")
+          toast.error("There was an error deleting your account. Please try again later or contact technical support.");
+          return;
         }
+
+        setDeleteDialogBox(false);
+        Logout();
+        navigate('/user-deleted');
       }
     }
 
@@ -53,6 +56,15 @@ export default function MyAccount(){
 
   return(
     <div className="account-container">
+      <ToastContainer
+        position="top-right"
+        autoClose={false}
+        newestOnTop={false}
+        closeOnClick
+        draggable
+        theme="colored"
+      />
+
       <h2>My Account</h2>
 
       <div className="account personal-info">
@@ -71,41 +83,53 @@ export default function MyAccount(){
         >
         Update Account
         </button>
-        <button className="big-btn danger" onClick={showDeleteConfirmation}>Permanently Delete...</button>
+        <button className="big-btn danger" onClick={showDeleteConfirmation}>Delete Account...</button>
       </div>
 
       {deleteDialogBox && 
         <dialog open className="delete-dialog">
           <h2>Confirm Account Deletion</h2>
-          <p>Are you sure you want to permanently delete your account?</p>
-          <p>You will forfeit all of your Sirch Coins (ⓢ 999) and will not be able to get them back. Those coins will be returned to the Sirch Coins total supply.</p>
-          <p>Your prior transactions affecting other users and the Sirch Coins total supply not be deleted.</p>
-          <p>This action cannot be undone. Once you delete your account, it is gone forever.</p>
+          <h4>Are you sure you want to <br></br><i>permanently delete</i> your account?</h4>
+          <ul>
+            <li>You will forfeit all of your ⓢ {userBalance?.balance} Sirch Coins and will not be able to get them back. Those coins will be returned to the Sirch Coins total supply.</li>
+            <li>Your prior transactions affecting other users and the Sirch Coins total supply not be deleted.</li>
+            <li>This action cannot be undone. Once you delete your account, it is gone forever.</li>
+          </ul>
           <div>
-            <p>Please enter your Account User Phrase to confirm this action:</p>
-            <input 
-              className="account-input"
-              type="text"
-              id="user-handle"
-              name="user-handle"
-              placeholder="Your User Phrase"
-              value={userHandle}
-              onChange={handleVerifyUserHandle}
-              required
-            />
-            <br></br>
-            <button
-              className="big-btn danger"
-              onClick={handleDeleteUser}
-              disabled={!isUserHandleVerified}
-            >
-              Yes, permanently delete
-            </button>
-            <button
-              className="big-btn"
-              onClick={() => setDeleteDialogBox(false)}>
-              Cancel
-            </button>
+            <h4>Please enter your two-word Account Handle to confirm this action:</h4>
+            <form onSubmit={handleDeleteUser} autoComplete="off">
+              <input
+                className="account-input"
+                type="text"
+                id="user-handle"
+                name="user-handle"
+                placeholder="Your User Handle"
+                value={userHandle}
+                onChange={handleVerifyUserHandle}
+                required
+              />
+              {userHandle && (
+                <p style={{ color: isUserHandleVerified ? "green" : "red" }}>
+                  {isUserHandleVerified ? "Verified!" : "Not Verified"}
+                </p>
+              )}
+
+              <br></br>
+
+              <button
+                className="big-btn danger"
+                type="submit"
+                // onClick={handleDeleteUser}
+                disabled={!isUserHandleVerified}
+              >
+                Yes, permanently delete
+              </button>
+              <button
+                className="big-btn"
+                onClick={() => setDeleteDialogBox(false)}>
+                Cancel
+              </button>
+            </form>
           </div>
         </dialog> 
       }
