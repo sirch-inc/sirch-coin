8633cb8b20beb2f684cfea32dedddebd06e5da06
Jeff Rink <jeffrinkdev@gmail.com>
2024-09-09 14:06:44 -0600

    flesh out delete user functionality (mostly working)

--- src/components/Account/MyAccount.jsx
+++ src/components/Account/MyAccount.jsx
@@ -7,6 +7,9 @@ import supabase from '../App/supabaseConfig';
 export default function MyAccount(){
   const [deleteDialogBox, setDeleteDialogBox] = useState(false);
   const { userInTable, userBalance } = useContext(AuthContext);
+  const [userHandle, setUserHandle] = useState('');
+  const [isUserHandleVerified, setIsUserHandleVerified] = useState(false);
+
   const navigate = useNavigate();
 
   function showDeleteConfirmation(){
@@ -17,27 +20,35 @@ export default function MyAccount(){
     navigate('/update-account');
   };
 
-  // TODO: Finish and test functions - on hold pending cascade discussions
-  function handleDelete(){
-    // eslint-disable-next-line no-unused-vars
-    const deleteUser = async (userInTable) => {
-      if (userInTable) {
-        // eslint-disable-next-line no-unused-vars
-        const { data, error } = await supabase 
-          .from('users')
-          .delete()
-          .eq('user_id', userInTable.user_id);
+  const handleVerifyUserHandle = (e) => {
+    const value = e.target.value;
 
-        if (error){
-          // TODO: Handle error
-          alert('Error deleting your account:\n' + error)
+    setUserHandle(value);
+    setIsUserHandleVerified(value === userInTable.user_handle);
+  };
+
+  // TODO: Finish and test functions - on hold pending cascade discussions
+  function handleDeleteUser() {    
+    const deleteUser = async () => {
+      if (userInTable) {
+        const { error } = await supabase.functions.invoke('delete-auth-user', {
+          body: {
+            user_id: userInTable.user_id
+          }
+        });
+
+        if (error) {
+          // TODO: surface error
+          alert("Error deleting your account:\n" + error)
         } else {
-          // TODO: Handle alert notification with Toast
+          // TODO: surface alert notification with Toast
           navigate('/')
           alert("Your account has been deleted.")
         }
       }
     }
+
+    deleteUser();
   }
 
   return(
@@ -67,13 +78,27 @@ export default function MyAccount(){
         <dialog open className="delete-dialog">
           <h2>Confirm Account Deletion</h2>
           <p>Are you sure you want to permanently delete your account?</p>
-          <p>You will forfeit all of your Sirch Coins and will not be able to get them back.</p>
-          <p>Your prior transactions affecting other users and the Sirch Coins ledger will not be deleted.</p>
+          <p>You will forfeit all of your Sirch Coins (ⓢ 999) and will not be able to get them back. Those coins will be returned to the Sirch Coins total supply.</p>
+          <p>Your prior transactions affecting other users and the Sirch Coins total supply not be deleted.</p>
           <p>This action cannot be undone. Once you delete your account, it is gone forever.</p>
           <div>
+            <p>Please enter your Account User Phrase to confirm this action:</p>
+            <input 
+              className="account-input"
+              type="text"
+              id="user-handle"
+              name="user-handle"
+              placeholder="Your User Phrase"
+              value={userHandle}
+              onChange={handleVerifyUserHandle}
+              required
+            />
+            <br></br>
             <button
               className="big-btn danger"
-              onClick={handleDelete}>
+              onClick={handleDeleteUser}
+              disabled={!isUserHandleVerified}
+            >
               Yes, permanently delete
             </button>
             <button
