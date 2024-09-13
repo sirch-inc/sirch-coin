9de6fead0995b1b75b4778ff439c7f12b24060a1
Jeff Rink <jeffrinkdev@gmail.com>
2024-09-13 00:25:22 -0600

    Update Send.jsx

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -6,7 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-// TODO: move this into its own component file with proper PROPs validation
+// TODO: move this into its own component file with proper react props validations
 export function UserCard({user, handleUserCardSelected}) {
   return (
     <div
