21e9c029c05b6a253e91725873e41bebe2c780cf
Jeff Rink <jeffrinkdev@gmail.com>
2024-09-13 15:49:55 -0600

    add a useDebounce module
    see https://www.developerway.com/posts/debouncing-in-react

---
+++ src/helpers/debounce.js
@@ -0,0 +1,23 @@
+import { useRef, useEffect, useMemo } from 'react';
+import debounce from "lodash.debounce";
+
+
+const useDebounce = (callback, debounceTimeMs = 1000) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  const debouncedCallback = useMemo(() => {
+    const func = () => {
+      ref.current?.();
+    };
+
+    return debounce(func, debounceTimeMs);
+  }, []);
+
+  return debouncedCallback;
+};
+
+export default useDebounce;
