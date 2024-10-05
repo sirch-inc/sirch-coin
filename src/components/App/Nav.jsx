9943f1825b175ead5cb6f35b7325dd3170ecef1d
Jeff Rink <jeffrinkdev@gmail.com>
2024-10-04 21:18:00 -0600

    quotes

--- src/components/App/Nav.jsx
+++ src/components/App/Nav.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LogoutSupabase from "../Account/Logout";
-import { AuthContext } from "../AuthContext";
+import LogoutSupabase from '../Account/Logout';
+import { AuthContext } from '../AuthContext';
 
 
 // eslint-disable-next-line react/prop-types
@@ -16,19 +16,19 @@ export default function NavBar({ supabase }) {
     
   return (
     <>
-      <ul className="navbar">
-        <li className="navbar-item">
-          <a href="/">
+      <ul className='navbar'>
+        <li className='navbar-item'>
+          <a href='/'>
             <img
-              src="/sirch_logo.png"
+              src='/sirch_logo.png'
               alt="Sirch Logo"
               // TODO: move this style into our CSS
-              style={{ width: "60px", height: "auto" }}
+              style={{ width: '60px', height: 'auto' }}
             />
           </a>
         </li>
-        <li className="navbar-item">
-          <a href="/">Sirch Coin</a>
+        <li className='navbar-item'>
+          <a href='/'>Sirch Coin</a>
         </li>
       </ul>
     </>
