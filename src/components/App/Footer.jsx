ff31e0a8172a5c74b2419d609e5d63ceba094524
Jeff Rink <jeffrinkdev@gmail.com>
2024-10-07 15:01:43 -0600

    move environment indicator into footer

--- src/components/App/Footer.jsx
+++ src/components/App/Footer.jsx
@@ -1,13 +1,26 @@
+import { Tooltip } from 'react-tooltip'
+
+
 export default function Footer() {
   return (
     <>
       <footer>
         <small><strong>
-        Copyright © 2024 Sirch Inc. All Rights Reserved.  
-        <span>
-          {"  " + import.meta.env.VITE_BUILD_VERSION}
-        </span>
+          Copyright © 2024 Sirch Inc. All Rights Reserved.  
+          <span className='environment-indicator'>
+            {"  " + import.meta.env.VITE_BUILD_VERSION}
+          </span>
+          {
+            import.meta.env.MODE !== 'production' &&    
+              <span>
+                { " (" + import.meta.env.MODE.toUpperCase() + ")" }
+              </span>
+          }
         </strong></small>
+
+        <Tooltip anchorSelect='.environment-indicator' clickable>
+          {import.meta.env.VITE_BUILD_VERSION_VERBOSE}
+        </Tooltip>
       </footer>
     </>
   );
