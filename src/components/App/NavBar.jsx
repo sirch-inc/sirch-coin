b7b3a2b64efa8675b54ff1ed96f591a7e02ac6ca
Jeff Rink <jeffrinkdev@gmail.com>
2024-10-16 15:42:54 -0600

    fixup navbar html

--- src/components/App/NavBar.jsx
+++ src/components/App/NavBar.jsx
@@ -9,11 +9,12 @@ export default function NavBar({ supabase }) {
   const { userInTable, userBalance } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  function handleLogout(){
-    LogoutSupabase({ supabase })
-    navigate('/')
-  }
-    
+  // TODO: remove this?
+  // function handleLogout(){
+  //   LogoutSupabase({ supabase })
+  //   navigate('/')
+  // }
+
   return (
     <nav>
       <ul className='navbar'>
@@ -21,9 +22,8 @@ export default function NavBar({ supabase }) {
           <a href='/'>
             <img
               src='/sirch-coin-favicon.ico'
-              alt="Sirch Logo"
-              // TODO: move this style into CSS
-              style={{ width: '60px', height: 'auto' }}
+              alt="Sirch Coins Logo"
+              className='sirchcoins-logo'
             />
           </a>
         </li>
