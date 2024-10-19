8a2315479fc291f838e2d53e24b89d908f79272c
Jeff Rink <jeffrinkdev@gmail.com>
2024-10-18 23:52:40 -0600

    remove header and footer from landing pages

--- src/components/App/LandingPages/ComingSoonApp.jsx
+++ src/components/App/LandingPages/ComingSoonApp.jsx
@@ -1,18 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from '../Header.jsx';
-import Footer from '../Footer.jsx';
 import ComingSoon from './ComingSoon.jsx';
 
 
 export default function ComingSoonApp() {
   return (
     <BrowserRouter>
-      <Header/>
       <Routes>
         <Route path='/' Component={ComingSoon}/>
       </Routes>
-      <Footer/>
     </BrowserRouter>
   );
 }
