70f12052bb8a6e83e16a7044c17c103333877bc7
Jeff Rink <jeffrinkdev@gmail.com>
2024-10-25 14:00:59 -0600

    simplify HTML and convert everything to grid to ensure equal columns, and add text-clipping with the ellipsis character (...)

--- src/components/TransactionHistory/TransactionCard.jsx
+++ src/components/TransactionHistory/TransactionCard.jsx
@@ -12,21 +12,11 @@ export default function TransactionCard({ date, type, amount, status, details })
 
   return (
     <div className='transaction-row'>
-      <div>
-        <p>{formatDate(date)}</p>
-      </div>
-      <div>
-        <p>{type}</p>
-      </div>
-      <div>
-        <p>ⓢ {amount}</p>
-      </div>
-      <div>
-        <p>{status}</p>
-      </div>
-      <div>
-        <p>{details}</p>
-      </div>
+      <p>{formatDate(date)}</p>
+      <p>{type}</p>
+      <p>ⓢ {amount}</p>
+      <p>{status}</p>
+      <p className='transaction-details'>{details}</p>
     </div>
   );
 }
