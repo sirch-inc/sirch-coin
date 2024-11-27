7d5e9a09d9df6acfeb5be395f5c9f102df5d5b7f
Jeff Rink <jeffrinkdev@gmail.com>
2024-11-26 17:09:08 -0700

    clean up DeleteUser view

--- src/components/Account/AccountDeleted.jsx
+++ src/components/Account/AccountDeleted.jsx
@@ -6,8 +6,8 @@ export default function AccountDeleted() {
     <>
       <h1>Your Sirch Coins account was successfully deleted.</h1>
       <h4>You may create a new account at any time.</h4>
-      <div className='button-container'>
-        <Link to='/' className='action-btn'>
+      <div className='bottom-btn-container'>
+        <Link to='/' className='big-btn'>
           Got it!
         </Link>
       </div>
