4d3ddaaf6d4cef26a1ed65aeb4132bdfa053614e
Jeff Rink <jeffrinkdev@gmail.com>
2024-12-16 18:12:52 -0700

    cleanup forgot-password

--- src/components/Account/ForgotPassword.jsx
+++ src/components/Account/ForgotPassword.jsx
@@ -36,23 +36,36 @@ export default function ForgotPassword() {
       <h1>Reset Password</h1>
       {/* TODO: Does this boolean switch even apply anymore since we now redirect on success of the request? */}
       {!resetSent ? (
-      <>
-        <p>Enter the email you used to sign up for Sirch Coin:</p>
-        <form className='reset-password' onSubmit={requestReset}>
-          <input
-            type='email'
-            placeholder="Email"
-            value={userEmail}
-            onChange={(e) => setUserEmail(e.target.value)}
-            required
-            autoComplete='username'
-          />
-          <button type='submit'>Send Reset Email</button>
-        </form>
-        {error && <p className='error'>{error}</p>}
-      </>
+        <>
+          <p>Enter the email address you used to sign up for Sirch Coin.</p>
+          <p>We will send you a reset-password email containing a link to conduct that process.</p>
+          <form className='reset-password' onSubmit={requestReset}>
+            <input
+              className='account-input'
+              type='email'
+              id='email'
+              name='email'
+              placeholder="Email"
+              value={userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
+              autoComplete='email'
+              required
+            />
+            <button className='account-button' type='submit'> Send Reset Email → </button>
+          </form>
+
+          {error &&
+            <p
+              style={{ color: 'red' }}
+              className='error'>
+              { error }
+            </p>}
+        </>
       ) : (
-      <p>We&apos;ve emailed {userEmail} a link to reset your password. Please check your email inbox.</p>
+        <>
+          <p>We&apos;ve emailed {userEmail} a link to reset your password. Please check your email inbox.</p>
+          <br/>
+        </>
       )}
 
       <div className='bottom-btn-container'>
