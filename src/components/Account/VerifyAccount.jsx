49694ad22eb082ceb9f309d4cef5f9b1589b042f
Jeff Rink <jeffrinkdev@gmail.com>
2024-12-17 15:05:20 -0700

    add back button to Verify account, and some session cleanups

--- src/components/Account/VerifyAccount.jsx
+++ src/components/Account/VerifyAccount.jsx
@@ -1,20 +1,36 @@
+import { useContext } from 'react';
+import { AuthContext } from '../AuthContext';
 import { Link } from 'react-router-dom';
 
 
 export default function VerifyAccount() {
+  const { session } = useContext(AuthContext);
+
   return(
-    // TODO: Style component and add customization (ie. "An email confirmation link was sent to [users@somewhere.com]")
-    // TODO: Only make page accessible to a newly created user
     <>
-      <h1>Please Check Your Email</h1>
-      <p>A verification email was sent to the address provided. Please click the link in that email to verify your new account.</p>
-      <p>To ensure your account&apos;s security, this process may take a few minutes.</p>
+      {
+        !session ? (
+          <>
+            <h1>Please Check Your Email</h1>
+            <p>A verification email was sent to the address provided.</p>
+            <p>Please click the link in that email to verify your new account.</p>
+            <p>To ensure your account&apos;s security, this process may take a few minutes.</p>
+        </>
+        ) : (
+          <>
+            <h2>You are currently logged in.</h2>
+            <br/>
+            <h3>You have already verified this account and no further action is necessary.</h3>
+            <br/>
+          </>
+        )
+      }
 
       <div style={{ textAlign: 'center', padding: '50px' }}>
         <Link to='/' className='big-btn'>
           Back to Home
         </Link>
       </div>
-</>
+    </>
   )
 }
