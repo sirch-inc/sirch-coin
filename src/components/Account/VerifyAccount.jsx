0f8dc87b6e1ed40d5585ec023a1ab739f0724bb7
Jeff Rink <jeffrinkdev@gmail.com>
2024-12-17 14:36:50 -0700

    cleanup Verify account and add Back-to-Home button

--- src/components/Account/VerifyAccount.jsx
+++ src/components/Account/VerifyAccount.jsx
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom';
+
+
 export default function VerifyAccount() {
   return(
     // TODO: Style component and add customization (ie. "An email confirmation link was sent to [users@somewhere.com]")
     // TODO: Only make page accessible to a newly created user
     <>
       <h1>Please Check Your Email</h1>
-      <h4>A verification email was sent to the address provided. Please click the link in that email to verify your new account.</h4>
-      <h4>To ensure your account&apos;s security, this process may take a few minutes.</h4>
-    </>
+      <p>A verification email was sent to the address provided. Please click the link in that email to verify your new account.</p>
+      <p>To ensure your account&apos;s security, this process may take a few minutes.</p>
+
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <Link to='/' className='big-btn'>
+          Back to Home
+        </Link>
+      </div>
+</>
   )
 }
