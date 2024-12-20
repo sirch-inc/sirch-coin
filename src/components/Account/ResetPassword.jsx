280ce483cad5e31ca0c94975a72f9c3f5373cda3
Jeff Rink <jeffrinkdev@gmail.com>
2024-12-20 16:29:30 -0700

    reset password WIP

--- src/components/Account/ResetPassword.jsx
+++ src/components/Account/ResetPassword.jsx
@@ -37,25 +37,31 @@ export default function ResetPassword() {
       alert("Passwords must match")
     }
 
-    // If the user has arrived at this page from their update password link, they should have a passwordRecoverySession
-    if (passwordRecoverySession) {
-      const { data, error } = await supabase.auth.updateUser(
-        { password: newPassword },
-        { session: passwordRecoverySession }
-      );
-      
-      if (error) {
-        // TODO: surface this error appropriately
-        alert('There was an error updating your password:\n' + error);
-        console.log("Data", data);
+    try {
+      // If the user has arrived at this page from their update password link, they should have a passwordRecoverySession
+      if (passwordRecoverySession) {
+        const { data, error } = await supabase.auth.updateUser(
+          { password: newPassword },
+          { session: passwordRecoverySession }
+        );
+        
+        if (error) {
+          // TODO: surface this error appropriately
+          alert('There was an error updating your password:\n' + error);
+          console.log("Data", data);
+        } else {
+          // TODO: surface this success message appropriately
+          alert('Password updated successfully!');
+          navigate('/');
+        }
       } else {
-        // TODO: surface this success message appropriately
-        alert('Password updated successfully!');
-        navigate('/');
+        // TODO: surface this error appropriately
+        alert("Something's not right... have you arrived at this page by clicking on the link in your email? You may need to request another password reset via the Login page before being able to successfully update your password.")
       }
-    } else {
-      // TODO: surface this error appropriately
-      alert("Something's not right... have you arrived at this page by clicking on the link in your email? You may need to request another password reset via the Login page before being able to successfully update your password.")
+    } catch(exception) {
+      console.error('Error:', exception);
+
+      navigate('/error', { replace: true });
     }
   }
 
