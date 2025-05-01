5e2eca91dba4d090822bc7f5f89f30133cbd70e7
Jeff Rink <jeffrinkdev@gmail.com>
2025-05-01 14:07:08 -0600

    move OfflineApp into its own folder

--- src/components/App/LandingPages/OfflinePage/OfflineApp/OfflineApp.jsx
+++ src/components/App/LandingPages/OfflinePage/OfflineApp/OfflineApp.jsx
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Offline from './Offline/Offline.jsx';
+import Offline from '../Offline/Offline.jsx';
+import './OfflineApp.css';
 
 
-export default function ComingSoonApp() {
+export default function OfflineApp() {
   return (
     <BrowserRouter>
       <Routes>
