655af30ae2ce4c84654950280aad701969673176
Jeff Rink <jeffrinkdev@gmail.com>
2025-05-01 15:01:17 -0600

    rename some page folders to indicate non-main

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client';
 
 
 // #v-ifdef VITE_IS_COMING_SOON.toLowerCase()
-import ComingSoonApp from './pages/ComingSoonPage/ComingSoonApp/ComingSoonApp';
+import ComingSoonApp from './pages/_ComingSoonPage/ComingSoonApp/ComingSoonApp';
 // #v-elif VITE_IS_OFFLINE.toLowerCase()
-import OfflineApp from './pages/OfflinePage/OfflineApp/OfflineApp';
+import OfflineApp from './pages/_OfflinePage/OfflineApp/OfflineApp';
 // #v-else
 import App from './pages/MainPage/App';
 // #v-endif
