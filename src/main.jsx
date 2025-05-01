86af22bfee649015dab7fa61e3686d309eeb5b6a
Jeff Rink <jeffrinkdev@gmail.com>
2025-05-01 15:05:34 -0600

    refactor naming

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom/client';
 
 
 // #v-ifdef VITE_IS_COMING_SOON.toLowerCase()
-import ComingSoonApp from './pages/_ComingSoonPage/ComingSoonApp/ComingSoonApp';
+import ComingSoonApp from './pages/_ComingSoon/ComingSoonApp/ComingSoonApp';
 // #v-elif VITE_IS_OFFLINE.toLowerCase()
-import OfflineApp from './pages/_OfflinePage/OfflineApp/OfflineApp';
+import OfflineApp from './pages/_Offline/OfflineApp/OfflineApp';
 // #v-else
-import App from './pages/MainPage/App';
+import App from './pages/Main/App';
 // #v-endif
 
 document.title = import.meta.env.VITE_PAGE_TITLE;
