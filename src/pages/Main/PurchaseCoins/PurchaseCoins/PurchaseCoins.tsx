c8a83ec017fad4573d8b9f4da650f8997f35f16c
Jeff Rink <jeffrinkdev@gmail.com>
2025-05-30 16:06:44 -0600

    WIP: remove strong

--- src/pages/Main/PurchaseCoins/PurchaseCoins/PurchaseCoins.tsx
+++ src/pages/Main/PurchaseCoins/PurchaseCoins/PurchaseCoins.tsx
@@ -130,9 +130,7 @@ export default function PurchaseCoins() {
           ? <p>Current quote: ⓢ 1 = Loading... {currency}</p>
           : <p>Current quote: ⓢ 1 = ${formatPrice(pricePerCoin)} {currency.toUpperCase()}</p>
         }
-        {/* TODO: use the fetched min value here... */}
-        <p>Note: a minimum purchase of ⓢ {minimumPurchase} is required.</p>
-
+        
         <div className='purchase-form'>
           <Input
             className='coin-input'
@@ -159,6 +157,8 @@ export default function PurchaseCoins() {
   
           />
         </div>
+        {/* TODO: use the fetched min value here... */}
+        <p>Note: At the current time, a minimum purchase of ⓢ {minimumPurchase} is required.</p>
         {/* TODO: Add "See more" link with info on Stripe/purchasing */}
         <p>Sirch Coins uses the payment provider Stripe for secure transactions. See more...</p>
         { localTotalPrice === 0
