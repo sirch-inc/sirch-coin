fd8c41c9ee358df97821ced82955dacbdcb6210c
Jeff Rink <jeffrinkdev@gmail.com>
2025-10-07 16:58:35 -0600

    remove unused files
    This reverts commit e9f2867aa9c1bd37dcfdb087ae0a266d683469a3.

--- src/pages/_ComingSoon/ComingSoonApp/ComingSoonApp.tsx
+++ src/pages/_ComingSoon/ComingSoonApp/ComingSoonApp.tsx
@@ -1,7 +1,6 @@
 import {HeroUIProvider} from "@heroui/react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ComingSoon from '../ComingSoon/ComingSoon';
-import './ComingSoonApp.css';
 
 // NOTE: This page is conditionally rendered in the root.jsx file based on the VITE_IS_COMING_SOON environment variable.
 export default function ComingSoonApp() {
